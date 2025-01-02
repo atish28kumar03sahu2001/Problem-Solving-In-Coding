97db7ec737b85c73522ec2d2a5be40ab9d10fedc
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-01-02 21:52:52 +0530

    Commit Java02

---
+++ Java/Java02.java
@@ -0,0 +1,10 @@
+// Create a program that print the phrase "Hello Java Program".
+public class Java02 {
+    public void Message() {
+        System.out.println("Hello Java Program"); // Hello Java Program
+    }
+    public static void main(String args[]) {
+        Java02 j = new Java02();
+        j.Message();
+    }
+}
