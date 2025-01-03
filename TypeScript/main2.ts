2ef743855b4cd9a200553a5ed4fb5c2bb582faaa
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-01-03 23:21:22 +0530

    Commit TypeScript03

---
+++ TypeScript/main2.ts
@@ -0,0 +1,5 @@
+// Input: Unlock the coding mysteries!	Output: Unlock the coding mysteries!
+function printMessage(message: string): void {
+    console.log(message);
+} printMessage("Unlock the coding mysteries!");
+// Output: Unlock the coding mysteries!
