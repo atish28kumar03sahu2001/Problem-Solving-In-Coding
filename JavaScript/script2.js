a370dd977a1f7c6da82b31be19dbe6a4c61b520d
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-01-03 23:20:24 +0530

    Commit JavaScript03

---
+++ JavaScript/script2.js
@@ -0,0 +1,5 @@
+// Input: Unlock the coding mysteries!	Output: Unlock the coding mysteries!
+function printMessage(message) {
+    console.log(message);
+} printMessage("Unlock the coding mysteries!");
+// Output: Unlock the coding mysteries!
