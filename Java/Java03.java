66f55aa4a9d31c07f502c2b0e7795b82ecd217d2
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-01-03 23:25:37 +0530

    Commit Java03

---
+++ Java/Java03.java
@@ -0,0 +1,10 @@
+// Input: Unlock the coding mysteries!	Output: Unlock the coding mysteries!
+public class Java03 {
+    public void Message(String message) {
+        System.out.println(message); // Unlock the coding mysteries!
+    }
+    public static void main(String args[]) {
+        Java03 j = new Java03();
+        j.Message("Unlock the coding mysteries!");
+    }
+}
