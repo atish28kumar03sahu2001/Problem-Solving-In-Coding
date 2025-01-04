01a4541c0393a1f7fbaf33ac0b9225ea3c958435
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-01-04 21:02:23 +0530

    Commit Java04

---
+++ Java/Java04.java
@@ -0,0 +1,10 @@
+// Input: a: 7 b: -3 Output: 4
+public class Java04 {
+    public void addNumber(int a, int b) {
+        System.out.println("a + b : "+(a+b));
+    }
+    public static void main(String args[]) {
+        Java04 j = new Java04();
+        j.addNumber(7, -3);
+    }
+}
